8d57fee8a074288a0a36b059333b0457f7021fd7
Erick Rivas <erick_rivas@outlook.com>
2019-05-10 03:39:17 -0500

    Add enum support

--- parser.js
+++ parser.js
@@ -1,16 +1,39 @@
-function parseMeta(model, attr)
+function parseMetas(models)
 {
-  let meta = attr.meta;
+  for (let model of models) {
+    mMeta = model.meta;
+    attrs = model.attrs;
+    model["meta"] = parseMeta(mMeta);
+    for (let attr of attrs) {
+      aMeta = attr.meta;
+      attr["meta"] = parseMeta(aMeta);
+    }
+  }
+  return models;
+}
+
+function parseMeta(meta)
+{
+  let res = null
   if (meta != "") {
     try {
       let meta_json = ""
       let props = meta.split("\n");
       for (let p of props)
         meta_json += `"${p.split(":")[0].trim()}": "${p.split(":")[1].trim()}"`
-      attr["meta"] = JSON.parse(`{${meta_json}}`);
-    } catch (e) { attr["meta"] = null; }
-  } else attr["meta"] = {};
-
+      res = JSON.parse(`{${meta_json}}`);
+      for (let m in res) {
+        if (res[m].startsWith("[")) {
+          let mrss = res[m].substring(1, res[m].length - 1)
+          let mrs = mrss.split(",")
+          res[m] = []
+          for (let mr of mrs)
+            res[m].push(String(mr.trim()))
+        }
+      }
+    } catch (e) { res = null; }
+  } else res = {};
+  return res;
 }
 
 function parseFks(model, attr, relations)
@@ -40,14 +63,14 @@ function walk(func, models, relations)
 
 function parse(models, relations)
 {
-  models = walk(parseMeta, models, relations)
-  models = walk(parseFks, models, relations)
-  return models
+  models = parseMetas(models);
+  models = walk(parseFks, models, relations);
+  return models;
 }
 
 
 
-let models = '[  {    "name": "user",    "attrs": [      {        "name": "email",        "type": "string",        "meta": "length: 512"      },      {        "name": "password",        "type": "string",        "meta": "length: 256"      },      {        "name": "teams",        "type": "team[]",        "meta": ""      }    ],    "meta": ""  },  {    "name": "team",    "attrs": [      {        "name": "name",        "type": "string",        "meta": "length: name"      },      {        "name": "logo_url",        "type": "string",        "meta": ""      },      {        "name": "players",        "type": "player[]",        "meta": ""      }    ],    "meta": ""  },  {    "name": "player",    "attrs": [      {        "name": "name",        "type": "string",        "meta": ""      },      {        "name": "photo_url",        "type": "string",        "meta": ""      }    ],    "meta": ""  },  {    "name": "match",    "attrs": [      {        "name": "date",        "type": "date",        "meta": ""      },      {        "name": "local",        "type": "team",        "meta": ""      },      {        "name": "visitor",        "type": "team",        "meta": ""      },      {        "name": "scores",        "type": "score[]",        "meta": ""      }    ],    "meta": ""  },  {    "name": "score",    "attrs": [      {        "name": "min",        "type": "int",        "meta": ""      },      {        "name": "player",        "type": "player",        "meta": ""      }    ],    "meta": ""  }]'
+let models = '[  {    "name": "user",    "attrs": [      {        "name": "email",        "type": "string",        "meta": "options: [Friendship, League, Cup]"      },      {        "name": "password",        "type": "string",        "meta": "length: 256"      },      {        "name": "teams",        "type": "team[]",        "meta": "options: [Friendship, League, Cup]"      }    ],    "meta": ""  },  {    "name": "team",    "attrs": [      {        "name": "name",        "type": "string",        "meta": "length: name"      },      {        "name": "logo_url",        "type": "string",        "meta": ""      },      {        "name": "players",        "type": "player[]",        "meta": ""      }    ],    "meta": ""  },  {    "name": "player",    "attrs": [      {        "name": "name",        "type": "string",        "meta": ""      },      {        "name": "photo_url",        "type": "string",        "meta": ""      }    ],    "meta": ""  },  {    "name": "match",    "attrs": [      {        "name": "date",        "type": "date",        "meta": ""      },      {        "name": "local",        "type": "team",        "meta": ""      },      {        "name": "visitor",        "type": "team",        "meta": ""      },      {        "name": "scores",        "type": "score[]",        "meta": ""      }    ],    "meta": ""  },  {    "name": "score",    "attrs": [      {        "name": "min",        "type": "int",        "meta": ""      },      {        "name": "player",        "type": "player",        "meta": ""      }    ],    "meta": ""  }]'
 let relations = '{  "user": {    "team": "0..*"  },  "team": {    "user": "0..*",    "player": "0..*",    "match": "0..*"  },  "player": {    "team": "1",    "score": "1"  },  "match": {    "team": "1",    "score": "0..*"  },  "score": {    "player": "1",    "match": "1"  }}';
 let res = parse(JSON.parse(models), JSON.parse(relations));
 console.log(JSON.stringify(res, null, 4));
--- validator.js
+++ validator.js
@@ -1,20 +1,26 @@
 function validateAttrs(models)
 {
-  let validTypes = ["int", "string", "date", "boolean", "float"]
+  let validTypes = ["int", "string", "date", "boolean", "float", "enum"]
   for (let model of models) {
     let mName = model.name;
     let attrs = model.attrs;
+    if (model.meta == null)
+        return `Invalid metadata model (${mName})<br/>TIP: Use format:<br/>name:value<br/>name:value`
     for (let attr of attrs) {
       if (attr.is_fk == false &&
         validTypes.indexOf(attr.type) == -1)
         return `Invalid type (${mName}.${attr.name}=${attr.type})<br/>Valid types: ${validTypes}`
       if (attr.meta == null)
-        return `Invalid metadata (${mName}.${attr.name})<br/>TIP: Use format:<br/>name:value<br/>name:value`
+        return `Invalid metadata (${mName}.${attr.name})<br/>TIP: Use format:<br/>name:value`
       if (attr.type == "string") {
         if (attr.meta.length == null)
           return `Include length meta in string(${mName}.${attr.name}) `
         if (Number.isNaN(parseInt(attr.meta.length)))
           return `Invalid length meta in string(${mName}.${attr.name}=${attr.meta.length}) `
+      }
+      if (attr.type == "enum"){
+        if (!Array.isArray(attr.meta.options))
+          return `Include options meta in enum (${mName}.${attr.name})<br/>Example:<br/>options:[b,c]`        
       }
     }
   }
@@ -55,7 +61,7 @@ function validate(models, relations)
   return errors;
 }
 
-let models = '[  {    "name": "user",    "attrs": [      {        "name": "email",        "type": "string",        "meta": {          "length": "512"        },        "is_fk": false,        "cardinality": ""      },      {        "name": "password",        "type": "string",        "meta": {          "length": "256"        },        "is_fk": false,        "cardinality": ""      },      {        "name": "teams",        "type": "team[]",        "meta": "puta",        "is_fk": true,        "cardinality": "0..*-0..*"      }    ],    "meta": ""  },  {    "name": "team",    "attrs": [      {        "name": "name",        "type": "string",        "meta": {          "length": "name"        },        "is_fk": false,        "cardinality": ""      },      {        "name": "logo_url",        "type": "string",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "players",        "type": "player[]",        "meta": {},        "is_fk": true,        "cardinality": "1-0..*"      }    ],    "meta": ""  },  {    "name": "player",    "attrs": [      {        "name": "name",        "type": "string",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "photo_url",        "type": "string",        "meta": {},        "is_fk": false,        "cardinality": ""      }    ],    "meta": ""  },  {    "name": "match",    "attrs": [      {        "name": "date",        "type": "date",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "local",        "type": "team",        "meta": {},        "is_fk": true,        "cardinality": "0..*-1"      },      {        "name": "visitor",        "type": "team",        "meta": {},        "is_fk": true,        "cardinality": "0..*-1"      },      {        "name": "scores",        "type": "score[]",        "meta": {},        "is_fk": true,        "cardinality": "1-0..*"      }    ],    "meta": ""  },  {    "name": "score",    "attrs": [      {        "name": "min",        "type": "int",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "player",        "type": "player",        "meta": {},        "is_fk": true,        "cardinality": "0..*-1"      }    ],    "meta": ""  }]'
+let models = '[  {    "name": "user",    "attrs": [      {        "name": "email",        "type": "string",        "meta": {          "length": "512"        },        "is_fk": false,        "cardinality": ""      },      {        "name": "password",        "type": "string",        "meta": {          "length": "256"        },        "is_fk": false,        "cardinality": ""      },      {        "name": "teams",        "type": "team[]",        "meta": "puta",        "is_fk": true,        "cardinality": "0..*-0..*"      }    ],    "meta": ""  },  {    "name": "team",    "attrs": [      {        "name": "name",        "type": "string",        "meta": {          "length": "123"        },        "is_fk": false,        "cardinality": ""      },      {        "name": "logo_url",        "type": "string",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "players",        "type": "player[]",        "meta": {},        "is_fk": true,        "cardinality": "1-0..*"      }    ],    "meta": ""  },  {    "name": "player",    "attrs": [      {        "name": "name",        "type": "string",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "photo_url",        "type": "string",        "meta": {},        "is_fk": false,        "cardinality": ""      }    ],    "meta": ""  },  {    "name": "match",    "attrs": [      {        "name": "date",        "type": "date",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "local",        "type": "team",        "meta": {},        "is_fk": true,        "cardinality": "0..*-1"      },      {        "name": "visitor",        "type": "team",        "meta": {},        "is_fk": true,        "cardinality": "0..*-1"      },      {        "name": "scores",        "type": "score[]",        "meta": {},        "is_fk": true,        "cardinality": "1-0..*"      }    ],    "meta": ""  },  {    "name": "score",    "attrs": [      {        "name": "min",        "type": "int",        "meta": {},        "is_fk": false,        "cardinality": ""      },      {        "name": "player",        "type": "player",        "meta": {},        "is_fk": true,        "cardinality": "0..*-1"      }    ],    "meta": ""  }]'
 let relations = '{  "user": {    "team": "0..*"  },  "team": {    "user": "0..*",    "player": "0..*",    "match": "0..*"  },  "player": {    "team": "1",    "score": "0..*"  },  "match": {    "team": "1",    "score": "0..*"  },  "score": {    "player": "1",    "match": "1"  }}'
 let res = validate(JSON.parse(models), JSON.parse(relations));
 console.log(JSON.stringify(res, null, 4));
